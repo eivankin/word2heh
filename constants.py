ff55ce1e49c9bdaf10e32c801ef3ca44f431cb24
Evgenij Ivankin <54205776+eivankin@users.noreply.github.com>
2022-01-28 09:55:11 +0300

    add missing 'ё'

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 DEBUG = True
 HEH_RATE = 1 if DEBUG else .3
 HEH_LEVEL = 1
-VOWELS: set[str] = set('уеыаоэяию')
+VOWELS: set[str] = set('уеыаоэяиюё')
 # Й, Ъ, Ь пропущены, так как с них не может начинаться слог
 CONSONANTS: set[str] = set('цкнгшщзхфвпрлджчсмтб')
