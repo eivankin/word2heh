c22b7687f294de3d7c1f5d054b34d0e80fb7d288
Evgenij Ivankin <e.ivankin@innopolis.university>
2024-02-15 19:25:11 +0300

    fix syllable search for non-lowercase & add test case

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 DEBUG = True
 HEH_RATE = 1 if DEBUG else .3
 HEH_LEVEL = 1
-VOWELS: set[str] = set('уеыаоэяиюё')
+VOWELS: set[str] = set('уеыаоэяиюё' + 'уеыаоэяиюё'.upper())
 # Й, Ъ, Ь пропущены, так как с них не может начинаться слог
-CONSONANTS: set[str] = set('цкнгшщзхфвпрлджчсмтб')
+CONSONANTS: set[str] = set('цкнгшщзхфвпрлджчсмтб' + 'цкнгшщзхфвпрлджчсмтб'.upper())
